6b4c29ca0bfa5fd34a509a9636843dc354991f85
Palaf <luis.pala1997@gmail>
2017-09-12 18:16:34 -0500

    100%

--- src/Lenguajes_Formales/Practica.java
+++ src/Lenguajes_Formales/Practica.java
@@ -9,42 +9,48 @@ public class Practica {
     public static void main(String[] args) {
         //VARIABLES
         Lenguaje lenguaje;
+        boolean estado=true;
         Scanner scanner = new Scanner(System.in);
         //CICLOS
-        while(true) {
-            System.out.println("Abecedario:");
-            //Lectura de datos
-            //Abecedario
-            String x1 = scanner.nextLine();
-            //Lenguaje
-            System.out.println("Lengueje:");
-            String x2 = scanner.nextLine();
-            //Se crea un nuevo lenguaje
-            lenguaje = new Lenguaje(x1, x2, "");
-            //Se checa el lenguaje con el abcedario
-            if (lenguaje.pertenece()) {
-                //Si se entra aqui, entonces el lenguaje no concuerda
-                continue;
-            }else {
-                break;
+        do{
+            while (true) {
+                System.out.println("Abecedario:");
+                //Lectura de datos
+                //Abecedario
+                String x1 = scanner.nextLine();
+                //Lenguaje
+                System.out.println("Lengueje:");
+                String x2 = scanner.nextLine();
+                //Se crea un nuevo lenguaje
+                lenguaje = new Lenguaje(x1, x2, "");
+                //Se checa el lenguaje con el abcedario
+                if (lenguaje.pertenece()) {
+                    //Si se entra aqui, entonces el lenguaje no concuerda
+                    continue;
+                } else {
+                    break;
+                }
             }
-        }
-        System.out.println("W:");
-         while(true) {
-             //W
-             String x3 = scanner.nextLine();
-             if (x3.equals("0")) {// System.out.println("¿Terminaste? 1(Si)");
-                 break;
-             }
-            //Se checa que el lenguaje coincida con W
-             lenguaje.setW(x3);
-            if (lenguaje.checar()) {
-                System.out.println("W:" + lenguaje.getW() + " es acapetada en L*:{" + lenguaje.getLenguajeString() + "}*");
-            } else {
-                System.out.println("W:" + lenguaje.getW() + " es rechazada en L*:{" + lenguaje.getLenguajeString() + "}*, " + "{" + lenguaje.getError() + "} no pertence a L");
+            System.out.println("W:");
+            while (true) {
+                //W
+                String x3 = scanner.nextLine();
+                if (x3.equals("1")) {// System.out.println("¿Terminaste? 1(Si)");
+                    estado=false;
+                    break;
+                } else if (x3.equals("0")) {
+                    break;
+                }else {
+                    //Se checa que el lenguaje coincida con W
+                    lenguaje.setW(x3);
+                    if (lenguaje.checar()) {
+                        System.out.println("W:" + lenguaje.getW() + " es acapetada en L*:{" + lenguaje.getLenguajeString() + "}*");
+                    } else {
+                        System.out.println("W:" + lenguaje.getW() + " es rechazada en L*:{" + lenguaje.getLenguajeString() + "}*, " + "{" + lenguaje.getError() + "} no pertence a L");
+                    }
+                }
             }
-
-        }
+    }while(estado);
 
 
     }
